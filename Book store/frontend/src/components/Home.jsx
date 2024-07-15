33b24fbb55de28024de2d4504a260609cbcc8b47
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2024-07-15 17:28:58 +0300

    done

--- Book store/frontend/src/components/Home.jsx
+++ Book store/frontend/src/components/Home.jsx
@@ -12,6 +12,10 @@ const Home = () => {
     const [books,setBooks]=useState([])
     const [loading,setloading]=useState(false)
 const[showType,setShowType]=useState('card')
+const [search,setsearch]=useState('')
+const[boook,setBoook]=useState()
+
+
 
     useEffect(()=>{
 setloading(true)
@@ -28,8 +32,26 @@ axios.get('http://localhost:5555/books/')
 })
     },[])
 
+    // useEffect(() => {
+    //     setBoook(books);
+    
+    //     const filteredData = books.filter((item) => item.title.toLowerCase().includes(search));
+    
+    //     setBoook(filteredData);
+    //   }, [books, search]);
+
+
+
   return (
     <div className='p-4 bg-gray-300'>
+        {/* <div className="search-crypto">
+          <input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setsearch(e.target.value.toLowerCase())}
+          />
+        </div> */}
+
+
         <div className="flex justify-center items-center gap-x-4 ">
             <button className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg" onClick={()=>setShowType('table')}>Table</button>
             <button className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg" onClick={()=>setShowType('card')}>Card</button>
--- Book store/frontend/src/home/card.jsx
+++ Book store/frontend/src/home/card.jsx
@@ -1,38 +1,11 @@
-import { Link } from 'react-router-dom'
-import {AiOutlineEdit} from 'react-icons/ai'
-import {PiBookOpenTextLight} from 'react-icons/pi'
-import {BiUserCircle} from 'react-icons/bi'
-import {BsInfoCircle} from 'react-icons/bs'
-import {MdOutlineDelete} from 'react-icons/md'
-
-
+import SingleBook from "../assets/SingleBook"
 const card = ({ books }) => {
   return (
     
    <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
     
-    {books.map((book,index)=>(
-        <div key={book._id} className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl">
-            <h1><strong>{index+1}</strong> </h1>
-            <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg text-blue-700"> <strong>{book.pageCount} Pages</strong> </h2>
-            <div><img src={book.thumbnailUrl}  />
-
-            </div>
-            <h4 className="my-2 text-gray-500 text-blue-700">{book.isbn}</h4>
-            <div className="flex justify-start items-center gap-x-2">
-                <PiBookOpenTextLight className='text-red-300 text-2xl'/>
-                <h2 className="my-1">{book.title}</h2>
-            </div>
-            <div className="flex justify-start items-center gap-x-2">
-                <BiUserCircle className='text-red-300 text-2xl'/>
-                <h2 className="my-1">{book.authors}</h2>
-            </div>
-            <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
-                <Link to={`/books/details/${book._id}`}><BsInfoCircle className='text-2xl text-green-800 hover:text-black'/></Link>
-                <Link to={`/books/edit/${book._id}`}><AiOutlineEdit className='text-2xl text-yellow-600 hover:text-black'/></Link>
-                <Link to={`/books/delete/${book._id}`}><MdOutlineDelete className='text-2xl text-red-600 hover:text-black'/></Link>
-            </div>
-        </div>
+    {books.map((item,index)=>(
+        <SingleBook key={item._id} book={item} index={index}/>
     ))}
    </div>
   )
